71303837b2b8b7c228e8a4c824b6505f242bc13e
Oli Slade <oliver.slade@digital.hmrc.gov.uk>
2018-06-21 13:01:48 +0100

    Adding cors to the application

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+var cors = require('cors')
 var express = require('express'),
-  app = express(),
-  routes = require("./api/routes/SkillsMatcherRoutes")
-  port = process.env.PORT || 3000;
+    app = express(),
+    routes = require("./api/routes/SkillsMatcherRoutes")
+port = process.env.PORT || 3000;
 
+
+app.use(cors())
 routes(app)
 app.listen(port);
-
 
 console.log('RESTful API server started on: ' + port);
