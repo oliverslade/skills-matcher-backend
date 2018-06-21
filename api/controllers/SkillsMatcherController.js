5172c2f5f7f76b6518ad2a63627f521fcab6d4d9
Oli Slade <oliver.slade@digital.hmrc.gov.uk>
2018-06-21 12:58:49 +0100

    Adding parameterisation to the jobs api

--- api/controllers/SkillsMatcherController.js
+++ api/controllers/SkillsMatcherController.js
@@ -1,7 +1,10 @@
 var axios = require('axios');
 
+var skill = "java"
+var location = "london"
+
 exports.jobs = async (req, res) => {
-    const {data} = await axios.get(`https://jobs.github.com/positions.json?description=python&location=new+york`);
+    const {data} = await axios.get(`https://jobs.github.com/positions.json?description=${skill}&location=${location}`);
 
     res.json(data);
 }
--- api/routes/SkillsMatcherRoutes.js
+++ api/routes/SkillsMatcherRoutes.js
@@ -1,8 +1,8 @@
 'use strict';
-module.exports = function(app) {
-  var controller = require('../controllers/SkillsMatcherController');
+module.exports = function (app) {
+    var controller = require('../controllers/SkillsMatcherController');
 
-  app.route('/submit')
-    .get(controller.jobs)
-    .post(controller.jobs)
+    app.route('/jobs')
+        .get(controller.jobs)
+        .post(controller.jobs)
 };
